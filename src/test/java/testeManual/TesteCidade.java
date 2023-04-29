d89955fb546b32e518f73927d43e7545fa0867b5
Gabriel Amoêdo <75450592+amoedogabriel@users.noreply.github.com>
2023-04-29 11:26:11 -0400

    atualizacao

--- src/test/java/testeManual/TesteCidade.java
+++ src/test/java/testeManual/TesteCidade.java
@@ -1,42 +1,105 @@
-package TesteManual;
+package testeManual;
 
-import model.Cidade;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
-import java.util.List;
+
+import model.Cidade;
+import util.JPAUtil;
 
 public class TesteCidade {
-    public static void main(String[] args) {
 
-//        Cidade cidade = new Cidade();
-//        cidade.setNome("Amazonas");
-//        cidade.setEstado("Manaus");
-//
-       EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("springtestes-jpa-pu");
-//
-       EntityManager em = fabrica.createEntityManager();
-//
-//        em.getTransaction().begin();
-//
-//        em.persist(cidade);
-//
-//        em.getTransaction().commit();
-//
 
+    private static void inserirNoBancoDeDados() {
+
+//        Obtenho a EntityManager
+        EntityManager em = JPAUtil.getEntityManager();
+
+        Cidade cidade = new Cidade();
+        cidade.setNome("Sao Paulo");
+        cidade.setEstado("Sao Paulo");
+
+        em.getTransaction().begin();
+
+        em.persist(cidade);
+
+        em.getTransaction().commit();
+
+        em.close();
+
+    }
+
+
+    private static void listarDoBancoDeDados() {
+
+        //      SQL (Structured Query Language): select id,nome,estado from cidade;
+//      JPQL (Java Persistence Query Language): select cidade from Cidade cidade
+
+        EntityManager em = JPAUtil.getEntityManager();
 
         Query consulta = em.createQuery("select cidade from Cidade cidade");
+
         List<Cidade> cidades = consulta.getResultList();
 
         for(Cidade c:cidades){
-            System.out.println("Nome: "+c.getNome()+" Estado:"+c.getEstado());
+            System.out.println("Id:"+c.getId()+" Nome:"+c.getNome()+" Estado:"+c.getEstado());
         }
 
         em.close();
 
+    }
+
+    private static void consultarEspecificoDoBancoDeDados() {
+
+        EntityManager em = JPAUtil.getEntityManager();
+
+        Cidade cidadeEncontrada = em.find(Cidade.class,5L);
+
+        System.out.println("Cidade encontrada:"+cidadeEncontrada.getNome());
+
+        em.close();
 
     }
+
+
+    private static void removerDoBancoDeDados() {
+
+        EntityManager em = JPAUtil.getEntityManager();
+
+        em.getTransaction().begin();
+
+        Cidade cidadeEncontrada=em.find(Cidade.class,5L);
+
+        em.remove(cidadeEncontrada);
+
+        em.getTransaction().commit();
+
+        em.close();
+
+    }
+
+    public static void main(String[] args) {
+
+        // Insere um registro no banco de dados.
+
+        inserirNoBancoDeDados();
+
+        // Listar os registros no banco de dados
+
+        listarDoBancoDeDados();
+
+        // Consultar um registro específico no banco de dados
+
+        consultarEspecificoDoBancoDeDados();
+
+        // Remover registros do banco de dados
+
+        removerDoBancoDeDados();
+
+    }
+
 
 }
