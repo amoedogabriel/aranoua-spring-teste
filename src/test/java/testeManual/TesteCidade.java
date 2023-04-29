3c19228a22486a6bef34fa2d7543535dafc6691b
Gabriel Amoêdo <75450592+amoedogabriel@users.noreply.github.com>
2023-04-29 09:54:05 -0400

    aula 02

--- src/test/java/testeManual/TesteCidade.java
+++ src/test/java/testeManual/TesteCidade.java
@@ -5,25 +5,38 @@ import model.Cidade;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
 
 public class TesteCidade {
     public static void main(String[] args) {
 
-        Cidade cidade = new Cidade();
-        cidade.setNome("Amazonas");
-        cidade.setEstado("Manaus");
+//        Cidade cidade = new Cidade();
+//        cidade.setNome("Amazonas");
+//        cidade.setEstado("Manaus");
+//
+       EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("springtestes-jpa-pu");
+//
+       EntityManager em = fabrica.createEntityManager();
+//
+//        em.getTransaction().begin();
+//
+//        em.persist(cidade);
+//
+//        em.getTransaction().commit();
+//
 
-        EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("springtestes-jpa-pu");
 
-        EntityManager em = fabrica.createEntityManager();
+        Query consulta = em.createQuery("select cidade from Cidade cidade");
+        List<Cidade> cidades = consulta.getResultList();
 
-        em.getTransaction().begin();
-
-        em.persist(cidade);
-
-        em.getTransaction().commit();
+        for(Cidade c:cidades){
+            System.out.println("Nome: "+c.getNome()+" Estado:"+c.getEstado());
+        }
 
         em.close();
+
+
     }
 
 }
